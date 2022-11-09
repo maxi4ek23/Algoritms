afb103f9363bb485f74a19e7d8920f9975097424
unknown <maxi4ek23@gmail.com>
2022-11-09 03:11:52 +0200

    add 4 lab code

--- main.py
+++ main.py
@@ -1,85 +1,82 @@
-
+import numpy
 from collections import defaultdict
 
 
-def read_file():
-    file = open('input1.txt')
-    lines = file.read()
-    count_line = len(lines.split('\n'))
-    string_arrays = []
-    for i in range(count_line):
-        string_arrays.append(lines.split('\n')[i].split(' '))
-    print(string_arrays)
-    vertices_array = []
-
-    for i in range(count_line):
-        for j in range(2):
-            vertices_array.append(string_arrays[i][j])
-
-    vertices_array = list(dict.fromkeys(vertices_array))
-    print(vertices_array)
-    vertice_number = len(vertices_array)
-    print(vertice_number)
-    number_arrays = []
-
-    for string_array in string_arrays:
-        help_array = []
-        for string_value in string_array:
-            integer_value = int(string_value)
-            help_array.append(integer_value)
-        number_arrays.append(help_array)
-
-    print(number_arrays)
-
-    return vertice_number, number_arrays
+def get_info_from_file():
+    file = open('input.txt')
+    lines = [line.rstrip() for line in file]
+    print(lines)
+    array_of_line = [list(i) for i in lines[1:]]
+    print(input)
+    width = int(lines[0].split(' ')[0])
+    print(width)
+    return array_of_line, width
 
 
-def output_cycle(boolean):
-    print(boolean)
-    # f = open('output.txt', 'x')
-    # f.write(str(boolean))
+def delete_not_finish_elements(array):
+    transposed_array = numpy.transpose(array)
+    print(transposed_array[-1:])
+    for not_finish_elements in transposed_array[-1:]:
+        not_finish_elements[1:-1] = ""
+    print(transposed_array)
+    return transposed_array
 
 
-class Graph:
-
-    def __init__(self, num_vertices):
-
-        self.V = num_vertices
-
-        self.graph = defaultdict(list)
-
-    def add_edge(self, array):
-
-        self.graph[array[0]].append(array[1])
-
-        self.graph[array[1]].append(array[0])
-
-    def recursive_cycle(self, v, visited, parent):
-        visited[v] = True
-        print(self.graph[v])
-        for i in self.graph[v]:
-            if not visited[i]:
-                if self.recursive_cycle(i, visited, v):
-                    return True
-            elif parent != i:
-                return True
-        return False
-
-    def has_cycle(self):
-        visited = [False] * self.V
-        for i in range(self.V):
-            if not visited[i]:
-                if self.recursive_cycle(i, visited, -1):
-                    return True
-        return False
+def find_unique_values(array):
+    unique_values_array = []
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            unique_values_array.append((array[i][j]))
+    unique_values = list(dict.fromkeys(unique_values_array))
+    return unique_values
 
 
-vertices, adj_list = read_file()
-g = Graph(vertices)
-for edge in adj_list:
-    g.add_edge(edge)
-if g.has_cycle():
-    print("Graph contains cycle")
-else:
-    print("Graph doesn't contain cycle ")
-output_cycle(g.has_cycle())
+def dist_for_element(matrix, width, unique_values):
+    current_route = defaultdict(list)
+    previous_route = defaultdict(list)
+
+    for unique_value in unique_values:
+        for b in range(width):
+            current_route[unique_value].append(0 * width)
+            previous_route[unique_value].append(0 * width)
+    print(current_route)
+    print(previous_route)
+
+    for i, i_value in enumerate(matrix):
+        for j, j_value in enumerate(i_value):
+            if not j_value:
+                continue
+            current_route[j_value][i] += 1
+
+            if i > 0 and j_value != matrix[i - 1][j]:
+                previous_route[j_value][i] += 1
+                previous_element_in_row = matrix[i - 1][j]
+                current_route[j_value][i] *= current_route[previous_element_in_row][i - 1]
+
+        if i == 0:
+            continue
+        for element in set(i_value):
+            print(sum((current_route[element])[:i]))
+            if element and sum((current_route[element])[:i]) != 0:
+                current_route[element][i] *= sum((current_route[element])[:i])
+                current_route[element][i] += previous_route[element][i]
+    return current_route
+
+
+def write_result(routes):
+    number_paths = numpy.sum([value[-1] for value in routes.values()])
+    print(number_paths)
+    f = open('output.txt', 'x')
+    f.write(str(number_paths))
+
+
+if __name__ == "__main__":
+    row_array, width = get_info_from_file()
+
+    edited_row_array = delete_not_finish_elements(row_array)
+
+    unique = find_unique_values(edited_row_array)
+
+    paths = dist_for_element(edited_row_array, width, unique)
+
+    write_result(paths)
